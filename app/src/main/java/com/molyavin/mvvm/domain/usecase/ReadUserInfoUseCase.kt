2713b52718e72d9e7be455843f7d93f207df3e04
Molyavin <molyavin.artom@gmail.com>
2023-08-03 11:16:46 +0300

    Bug fixes

--- app/src/main/java/com/molyavin/mvvm/domain/usecase/ReadUserInfoUseCase.kt
+++ app/src/main/java/com/molyavin/mvvm/domain/usecase/ReadUserInfoUseCase.kt
@@ -4,7 +4,7 @@ import com.molyavin.mvvm.domain.models.UserInfo
 import com.molyavin.mvvm.data.repositories.UserRepository
 import javax.inject.Inject
 
-class ReadUserInfoUseCase @Inject constructor(private val userRepository: UserRepository) :
+class ReadUserInfoUseCase (private val userRepository: UserRepository) :
     IUseCase.IReadUserInfoUseCase {
     override fun execute(income: Any?): UserInfo {
         return userRepository.readData()
