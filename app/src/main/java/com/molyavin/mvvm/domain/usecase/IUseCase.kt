3b9a1e8c4abe949be2ba076d902d90d80dd67cfa
Molyavin <molyavin.artom@gmail.com>
2023-08-09 09:56:39 +0300

    Bug fixes v4

--- app/src/main/java/com/molyavin/mvvm/domain/usecase/IUseCase.kt
+++ app/src/main/java/com/molyavin/mvvm/domain/usecase/IUseCase.kt
@@ -6,4 +6,6 @@ interface IUseCase<T, R> {
     fun execute(income: T): R
     interface IReadUserInfoUseCase : IUseCase<Any?, UserInfo>
     interface ISaveUserInfoUseCase : IUseCase<UserInfo, Unit>
+
+    interface ICheckFieldUseCase:IUseCase<UserInfo, Any?>
 }
