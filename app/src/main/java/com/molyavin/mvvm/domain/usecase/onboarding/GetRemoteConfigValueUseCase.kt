3101749a57c518d84b0bdbb8b0dc4e2f1506b27f
Artem Molyavin <molyavin@Artems-MacBook-Pro.local>
2023-09-05 11:20:22 +0300

    Rewritten receive implementation remote config with coroutine

--- app/src/main/java/com/molyavin/mvvm/domain/usecase/onboarding/GetRemoteConfigValueUseCase.kt
+++ app/src/main/java/com/molyavin/mvvm/domain/usecase/onboarding/GetRemoteConfigValueUseCase.kt
@@ -1,30 +1,33 @@
 package com.molyavin.mvvm.domain.usecase.onboarding
 
-import android.util.Log
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
-import com.molyavin.mvvm.domain.usecase.base.IUseCase
+import com.molyavin.mvvm.domain.usecase.base.IAsyncUseCase
 import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 class GetRemoteConfigValueUseCase @Inject constructor(
     private val firebaseRemoteConfig: FirebaseRemoteConfig
-) : IUseCase<String, Boolean> {
+) : IAsyncUseCase<String, Boolean> {
 
-    override fun execute(income: String): Boolean {
+    override suspend fun execute(income: String): Boolean = suspendCoroutine { continuation ->
 
         val configSettings = FirebaseRemoteConfigSettings.Builder()
-            .setMinimumFetchIntervalInSeconds(20)
+            .setMinimumFetchIntervalInSeconds(0)
             .build()
         firebaseRemoteConfig.setConfigSettingsAsync(configSettings)
 
         firebaseRemoteConfig.fetch().addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 firebaseRemoteConfig.activate()
+                val value = firebaseRemoteConfig.getBoolean(income)
+                continuation.resume(value)
             } else {
-                Log.d("RemoteConfigException", task.exception.toString())
+                continuation.resumeWithException(task.exception ?: Exception("Fetch failed"))
             }
         }
-
-        return firebaseRemoteConfig.getBoolean(income)
     }
+
 }
--- app/src/main/java/com/molyavin/mvvm/domain/usecase/onboarding/GetStatusOnBoardingUseCase.kt
+++ app/src/main/java/com/molyavin/mvvm/domain/usecase/onboarding/GetStatusOnBoardingUseCase.kt
@@ -1,20 +1,21 @@
 package com.molyavin.mvvm.domain.usecase.onboarding
 
 import com.molyavin.mvvm.data.repositories.SettingRepository
-import com.molyavin.mvvm.domain.usecase.base.IUseCase
+import com.molyavin.mvvm.domain.usecase.base.IAsyncUseCase
 import javax.inject.Inject
 
 class GetStatusOnBoardingUseCase @Inject constructor(
     private val settingRepository: SettingRepository,
     private val getRemoteConfigValueUseCase: GetRemoteConfigValueUseCase
 ) :
-    IUseCase<Any?, Boolean> {
+    IAsyncUseCase<Any?, Boolean> {
 
-    override fun execute(income: Any?): Boolean {
+    override suspend fun execute(income: Any?): Boolean {
 
         if (getRemoteConfigValueUseCase.execute("on_boarding")) {
-            return settingRepository.readSetting("OnBoarding")
+            return true
         }
-        return true
+
+        return settingRepository.readSetting("OnBoarding")
     }
 }
